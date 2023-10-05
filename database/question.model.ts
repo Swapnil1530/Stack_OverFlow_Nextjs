5eb51eb7468702b795b1c8f91d5b1b64382e9bbf
Patel Swapnil <patelswapnil1004@gmail.com>
2023-10-05 18:19:00 +0530

    feat :add question tag details page

--- database/question.model.ts
+++ database/question.model.ts
@@ -5,8 +5,8 @@ export interface IQuestion extends Document {
   content: string;
   tags: Schema.Types.ObjectId[];
   views: number;
-  upVotes: Schema.Types.ObjectId[];
-  downVotes: Schema.Types.ObjectId[];
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
   author: Schema.Types.ObjectId;
   answers: Schema.Types.ObjectId[];
   createdAt: Date;
