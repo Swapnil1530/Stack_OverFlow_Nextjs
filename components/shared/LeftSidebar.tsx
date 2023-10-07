fae9ee522808c8a341271d3c20a8d1d98fecfd04
Patel Swapnil <patelswapnil1004@gmail.com>
2023-10-07 18:24:02 +0530

    [feat] : add hotQuestion & popularTag in home page ✨

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -13,7 +13,7 @@ const LeftSidebar = () => {
 
   return (
     <section className="background-light900_dark200 border-yellow-600 custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-32 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <div className="flex flex-1 flex-col gap-3">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
