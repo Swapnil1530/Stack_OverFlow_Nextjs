e3fadf454f463e05ddcf5a07b9e5ec54720f1f52
Patel Swapnil <patelswapnil1004@gmail.com>
2023-10-12 17:42:06 +0530

    [ fix ] : fix error in middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "/question/:id",
+    "question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
@@ -15,5 +15,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
