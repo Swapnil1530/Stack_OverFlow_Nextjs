628859788d07e1bbee99540046a47c5107bd2874
Patel Swapnil <patelswapnil1004@gmail.com>
2023-10-13 16:57:22 +0530

    change database name

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -15,7 +15,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "demonext",
+      dbName: "nextoverflow",
     });
 
     isConnected = true;
